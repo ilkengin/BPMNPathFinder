819b5b92f794848b1189e9b3ca67ab3e70d9665f
Ilkan Engin <ilkan.engin@siemens.com>
2020-09-15 22:01:37 +0300

    Refactored main method to call necessary service methods

--- src/main/java/org/ilkengin/bpmnpathfinder/Application.java
+++ src/main/java/org/ilkengin/bpmnpathfinder/Application.java
@@ -1,19 +1,48 @@
 package org.ilkengin.bpmnpathfinder;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.util.List;
 
-import org.ilkengin.bpmnpathfinder.config.RetrofitConfig;
+import org.camunda.bpm.model.bpmn.Bpmn;
+import org.camunda.bpm.model.bpmn.BpmnModelInstance;
+import org.ilkengin.bpmnpathfinder.config.AppConfig;
 import org.ilkengin.bpmnpathfinder.model.BpmnResponse;
+import org.ilkengin.bpmnpathfinder.service.BpmnTraverserService;
 
 import retrofit2.Response;
 
 public class Application {
 
 	public static void main(String[] args) {
+		if (args.length != 2) {
+			System.out.println("Start and target nodeID's should be supplied!");
+			System.exit(-1);
+		}
+		
+		String startNodeId = args[0];
+		String targetNodeId = args[1];
+		
 		try {
-			Response<BpmnResponse> response = RetrofitConfig.getBpmnService().getBpmn().execute();
+			// Get BPMN from server
+			Response<BpmnResponse> response = AppConfig.constructBpmnService().getBpmn().execute();
+			
+			// Build the model instance
+			BpmnModelInstance modelInstance = Bpmn.readModelFromStream(new ByteArrayInputStream(response.body().getBpmn20Xml().getBytes()));
+			
+			// Get Traverser Service
+			BpmnTraverserService bpmnTraverserService = AppConfig.getBpmnTraverser(modelInstance);
+			
+			List<String> inBetweenNodeIds = bpmnTraverserService.findPathBetween(startNodeId, targetNodeId);
+			if (inBetweenNodeIds != null) {				
+				inBetweenNodeIds.forEach(System.out::println);
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
+			System.exit(-1);
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(-1);
 		}
 	}
 }
--- src/main/java/org/ilkengin/bpmnpathfinder/config/AppConfig.java
+++ src/main/java/org/ilkengin/bpmnpathfinder/config/AppConfig.java
@@ -1,18 +1,25 @@
 package org.ilkengin.bpmnpathfinder.config;
 
+import org.camunda.bpm.model.bpmn.BpmnModelInstance;
 import org.ilkengin.bpmnpathfinder.service.BpmnService;
+import org.ilkengin.bpmnpathfinder.service.BpmnTraverserService;
 
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class RetrofitConfig {
+public class AppConfig {
 	
-	public static BpmnService getBpmnService() {
+	public static BpmnService constructBpmnService() {
+
 		Retrofit retrofit = new Retrofit.Builder()
 			    .baseUrl("https://n35ro2ic4d.execute-api.eu-central-1.amazonaws.com/")
 			    .addConverterFactory(GsonConverterFactory.create())
 			    .build();
 
 		return retrofit.create(BpmnService.class);
+	}
+	
+	public static BpmnTraverserService getBpmnTraverser(BpmnModelInstance bpmnModelInstance) {
+		return new BpmnTraverserService(bpmnModelInstance);
 	}
 }
