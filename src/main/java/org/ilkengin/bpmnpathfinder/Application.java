c876472904011fb3feebe746c27654381925f226
Ilkan Engin <ilkan.engin@siemens.com>
2020-09-15 21:14:23 +0300

    Added http call to application main method

--- src/main/java/org/ilkengin/bpmnpathfinder/Application.java
+++ src/main/java/org/ilkengin/bpmnpathfinder/Application.java
@@ -1,9 +1,19 @@
 package org.ilkengin.bpmnpathfinder;
 
+import java.io.IOException;
+
+import org.ilkengin.bpmnpathfinder.config.RetrofitConfig;
+import org.ilkengin.bpmnpathfinder.model.BpmnResponse;
+
+import retrofit2.Response;
+
 public class Application {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
+		try {
+			Response<BpmnResponse> response = RetrofitConfig.getBpmnService().getBpmn().execute();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-
 }
